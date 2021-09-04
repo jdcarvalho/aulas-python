adc3b082da707d49928a30e44bfe3215620100ef
João Carvalho <joao.carvalho@maestrus.com>
2021-09-04 12:13:32 -0300

    Funcionalidade b

---
+++ aulas-python/3_git/func_a.py
@@ -0,0 +1,18 @@
+
+"""
+Este arquivo será responsável por fazer a funcionalidade de merge entre duas
+branches.. pererê parará caixinha de fósforo
+
+"""
+
+
+def raiz_quadrada(numero):
+    import math
+    print(math.sqrt(numero))
+
+
+def ola_mundo(nome_bonito):
+    """"
+    Documentação do código.
+    """
+    print(f"Olá fulaninho seu nome {nome_bonito} é muito legal.")
